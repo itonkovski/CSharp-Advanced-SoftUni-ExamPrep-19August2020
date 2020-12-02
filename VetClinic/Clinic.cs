17700d4ad684b5cec8222a6bb566b9e2de1d7542
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2020-12-02 12:52:36 +0100

    VetClinic
    Final 100/100

---
+++ VetClinic/Clinic.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        private List<Pet> data;
+
+        public Clinic()
+        {
+            this.data = new List<Pet>();
+        }
+
+        public Clinic(int capacity)
+            :this()
+        {
+            this.Capacity = capacity;
+        }
+
+        public int Capacity { get; set; }
+
+        public int Count => this.data.Count;
+
+        public void Add(Pet pet)
+        {
+            if (this.data.Count + 1 <= this.Capacity)
+            {
+                this.data.Add(pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            Pet pet = this.data
+                .FirstOrDefault(p => p.Name == name);
+
+            if (pet != null)
+            {
+                this.data.Remove(pet);
+                return true;
+            }
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            Pet pet = this.data
+                .FirstOrDefault(p => (p.Name == name && p.Owner == owner));
+
+            if (pet != null)
+            {
+                return pet;
+            }
+            return null;
+        }
+
+        public Pet GetOldestPet()
+        {
+            Pet oldestPet = this.data
+                .OrderByDescending(p => p.Age)
+                .FirstOrDefault();
+
+            return oldestPet;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb
+                .AppendLine($"The clinic has the following patients:");
+
+            foreach (var pet in this.data)
+            {
+                sb
+                    .AppendLine($"Pet {pet.Name} with owner: {pet.Owner}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ VetClinic/Pet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Pet
+    {
+        public Pet(string name, int age, string owner)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Owner = owner;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Owner { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb
+                .AppendLine($"Name: {this.Name} Age: {this.Age} Owner: {this.Owner}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
